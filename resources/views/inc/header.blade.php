e52deecf2a38363c7cd46ecfa864139c8129f34c
DhruvSvt <dhruvsvt08@gmail.com>
2024-01-23 17:42:19 +0530

    nav menu

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -27,7 +27,52 @@
                         <li><a href="#">Dropdown 4</a></li>
                     </ul>
                 </li>
-                <li class="dropdown has-dropdown"><a href="#"><span>Workspace</span><i
+                <li class="dropdown has-dropdown workspace">
+                    <a href="#">
+                        <span>Workspace</span>
+                        <i class="fa-solid fa-caret-down"></i>
+                    </a>
+                    <ul class="dd-box-shadow">
+                        <div class="workspace-box col-12">
+                            <div class="col-4">
+                                <li><a href="#" class="heading">Tailored Office Spaces to
+                                        <br>Match <span>Your Needs.</span></a></li>
+                                <li><img src="{{ config('app.url') }}/assets/img/nav-img.png" alt="" srcset="">
+                                </li>
+                            </div>
+                            <div class="col-4">
+                                <li><a href="#" class="item">COWORKING
+                                        SPACE</a>
+                                    <p style="font-size: 11px; font-weight:400;color:#000;padding-left:11px">Collaborate
+                                        and
+                                        Create: Your Ideal
+                                        Shared Office Space Awaits!</p>
+                                </li>
+                                <hr style="margin-top: 20px;margin-left: 11px;width: 19rem;border-top: 1px solid #000;">
+                                <li style="font-size: 14px; font-weight:700"><a href="#" class="item">PRIVATE OFFICE SPACE</a>
+                                    <p style="font-size: 11px; font-weight:400;color:#000;padding-left:11px">Empower
+                                        Your Workspace: Discover the Advantage of Managed Office Spaces</p>
+                                </li>
+                                <hr style="margin-top: 15px;margin-left: 11px;width: 19rem;border-top: 1px solid #000;">
+                            </div>
+                            <div class="col-4">
+                                <li style="font-size: 14px; font-weight:700"><a href="#" class="item">MEETING ROOMS</a>
+                                    <p style="font-size: 11px; font-weight:400;color:#000;padding-left:11px">
+                                        Empower Your Meetings with Exceptional Spaces
+                                    </p>
+                                </li>
+                                <hr style="margin-top: 20px;margin-left: 11px;width: 19rem;border-top: 1px solid #000;">
+                                <li style="font-size: 14px; font-weight:700"><a href="#" class="item">VIRTUAL OFFICE SERVICE</a>
+                                    <p style="font-size: 11px; font-weight:400;color:#000;padding-left:11px">
+                                        Your Office, Your Way: Virtually Redefining Workspaces.
+                                    </p>
+                                </li>
+                                <hr style="margin-top: 30px;margin-left: 11px;width: 19rem;border-top: 1px solid #000;">
+                            </div>
+                        </div>
+                    </ul>
+                </li>
+                {{-- <li class="dropdown has-dropdown"><a href="#"><span>Workspace</span><i
                             class="fa-solid fa-caret-down"></i></a>
                     <ul class="dd-box-shadow">
                         <li><a href="#">Dropdown 1</a></li>
@@ -45,7 +90,7 @@
                         <li><a href="#">Dropdown 3</a></li>
                         <li><a href="#">Dropdown 4</a></li>
                     </ul>
-                </li>
+                </li> --}}
                 <li><a href="index.html#contact">Gallery</a></li>
             </ul>
 
